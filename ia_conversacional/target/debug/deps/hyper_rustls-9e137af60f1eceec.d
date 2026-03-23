c00374ae9e5c5f09b40da767cdf31e31c22c8d8f
omar-2604 <omarelghoulb26@gmail.com>
2026-03-23 14:30:57 +0100

    update structure

---
+++ ia_conversacional/target/debug/deps/hyper_rustls-9e137af60f1eceec.d
@@ -0,0 +1,9 @@
+C:\Users\lahcen\Desktop\Entornos del desarrollo\Chatbot\chatbot-gitanillo\ia_conversacional\target\debug\deps\hyper_rustls-9e137af60f1eceec.d: C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\lib.rs C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\config.rs C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\connector.rs C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\connector\builder.rs C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\stream.rs
+
+C:\Users\lahcen\Desktop\Entornos del desarrollo\Chatbot\chatbot-gitanillo\ia_conversacional\target\debug\deps\libhyper_rustls-9e137af60f1eceec.rmeta: C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\lib.rs C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\config.rs C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\connector.rs C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\connector\builder.rs C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\stream.rs
+
+C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\lib.rs:
+C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\config.rs:
+C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\connector.rs:
+C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\connector\builder.rs:
+C:\Users\lahcen\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hyper-rustls-0.27.7\src\stream.rs:
---
+++ ia_conversacional/target/debug/deps/ia_conversacional-3bc893781cf4a762.d
@@ -0,0 +1,5 @@
+C:\Users\lahcen\Desktop\Entornos del desarrollo\Chatbot\chatbot-gitanillo\ia_conversacional\target\debug\deps\ia_conversacional-3bc893781cf4a762.d: src\main.rs
+
+C:\Users\lahcen\Desktop\Entornos del desarrollo\Chatbot\chatbot-gitanillo\ia_conversacional\target\debug\deps\libia_conversacional-3bc893781cf4a762.rmeta: src\main.rs
+
+src\main.rs:
